74102891ad6bafa422664605464d2174a6bf597e
Abdullahi254 <abdush504@gmail.com>
2022-11-29 13:14:53 +0300

    fixed bug issue in sanity entry script

--- sanity.ts
+++ sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID||"project",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ||"production",
     apiVersion: "2022-03-25",
     useCdn: process.env.NODE_ENV === "production"
