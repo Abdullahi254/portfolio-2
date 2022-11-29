cd499ba38f6c2c2d05ee55f022f2eb37a609f6d7
Abdullahi254 <abdush504@gmail.com>
2022-11-29 13:09:30 +0300

    fixed bug

--- sanity.ts
+++ sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID||"fopdexnr",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ||"production",
     apiVersion: "2022-03-25",
     useCdn: process.env.NODE_ENV === "production"
